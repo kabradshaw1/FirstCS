a376c9d42914e87dce7618ea734ba9f1ace064c1
Kyle Bradshaw <itllbok82@gmail.com>
2026-03-15 19:13:33 -0600

    working on basics

--- FirstApi/Program.cs
+++ FirstApi/Program.cs
@@ -33,9 +33,39 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+// This is the first example endpoint.  When the server runs, putting 
+// http://localhost:5000/hello in the browser will return the string "Hello Kyle".  
 app.MapGet("/hello", () => "Hello Kyle");
 
+// The second example endpoint is a parameterized version of the first.  
+// Putting http://localhost:5000/hello/Kyle in the browser will return the 
+// same result as the first endpoint, but putting http://localhost:5000/hello/Alice 
+// will return "Hello Alice".
 app.MapGet("/hello/{name}", (string name) => $"Hello {name}");
+
+// This example servers to show how to return a JSON object.  
+// Putting http://localhost:5000/user in the browser will return 
+// a JSON object with three properties: id, name, and role.
+// still no logic here, just returning a hard coded object, 
+// but you can imagine how this could be used to return a 
+// user object from a database or some other source.
+app.MapGet("/user", () =>
+{
+    return new
+    {
+        id = 1,
+        name = "Kyle",
+        role = "developer"
+    };
+});
+
+// This example shows the use of query parameters.  Putting http://localhost:5000/add?a=5&b=10
+//  in the browser will return 15.  This is another way to pass information to the database,
+// instead of putting them in a json.  It also starts to demonstrate some of the syntax of C#, 
+// such as lambda expressions and type inference.  The parameters a and b are automatically 
+// parsed from the query string and passed to the lambda function, which returns their sum.
+app.MapGet("/add", (int a, int b) => a + b);
+
 
 app.Run();
 
