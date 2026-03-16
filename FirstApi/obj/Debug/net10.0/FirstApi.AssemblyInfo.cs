e37daaf2a55eb1b9e255bf9ed4c2ce14d7d40560
Kyle Bradshaw <itllbok82@gmail.com>
2026-03-15 20:48:36 -0600

    moving into services

--- FirstApi/obj/Debug/net10.0/FirstApi.AssemblyInfo.cs
+++ FirstApi/obj/Debug/net10.0/FirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d276b77c4bf230ba29c603abce67409e2debae4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd3d9f67e7ebce86c00a253a9e724d39b7e7655")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SecondApi/Services/UserService.cs
@@ -0,0 +1,26 @@
+/* 
+This is an example of simple class in C#. Classes are a fundemental building
+block of object oriented programming, and they allow you to create your own data types
+and define the properties and methods that they have. 
+*/
+public class UserService
+{
+    /*
+        
+    */
+    private static List<User> users = new()
+    {
+        new User("Kyle", 30),
+        new User("Alice", 25)
+    };
+
+    public List<User> GetUsers()
+    {
+        return users;
+    }
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+}
